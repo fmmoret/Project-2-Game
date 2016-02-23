b2ba77d519a9470856838de8bb5d7485c57d4a2c
Eric Schmidt <easchmidtjr@gmail.com>
2016-02-22 23:37:56 -0500

    Created loadLevel function

--- src/game.js
+++ src/game.js
@@ -13,6 +13,7 @@
     var game = new Phaser.Game(stageWidth, stageHeight, Phaser.AUTO, 'game', {preload: preload, create: create, update: update});
     var player;
     var goal;
+    var levelIndex;
     var platforms;
     var downArrow;
     var cursors;
@@ -22,8 +23,6 @@
     var gravityDirection = {x: 0, y: 1};
 
     // Constants
-    var GOAL_POSITION = {x: 410, y: 130};
-    var START_POSITION = {x: 50, y: stageHeight - 70};
     var GRAVITY_STRENGTH = 500;
     var JUMP_VELOCITY = 300;
     var MOVE_VELOCITY = 250;
@@ -31,51 +30,6 @@
     var WARNING_TIME = 3000;
     var GRAVITY_DIRECTIONS = [{x: 1, y: 0}, {x: -1, y: 0}, {x: 0, y: 1}, {x: 0, y: -1}];
     var ROTATIONS = [Math.PI*3.0/2.0, Math.PI/2, 0, Math.PI];
-
-    // The platforms
-    // #1
-    /*var PLATFORMS = [
-        {x: 0, y: 0, width: stageWidth, height: 20},
-        {x: 0, y: 0, width: 20, height: stageHeight},
-        {x: stageWidth-20, y: 0, width: 20, height: stageHeight},
-        {x: 0, y: stageHeight-20, width: stageWidth, height: 20},
-        {x: 150, y: 420, width: 100, height: 50},
-        {x: 500, y: 350, width: 75, height: 100},
-        {x: 300, y: 100, width: 200, height: 25},
-        {x: 90, y: 200, width: 150, height: 100},
-        {x: 350, y: 250, width: 100, height: 100},
-        {x: 650, y: 200, width: 50, height: 150},
-    ];*/
-    // #2
-    var PLATFORMS = [
-        {x: 0, y: 0, width: stageWidth, height: 20},
-        {x: 0, y: 0, width: 20, height: stageHeight},
-        {x: stageWidth-20, y: 0, width: 20, height: stageHeight},
-        {x: 0, y: stageHeight-20, width: stageWidth, height: 20},
-
-        {x: 325, y: 100, width: 175, height: 25},
-        {x: 325, y: 250, width: 175, height: 25},
-        {x: 500, y: 100, width: 25, height: 175},
-        {x: 250, y: 180, width: 200, height: 10},
-
-        {x: 225, y: 0, width: 25, height: 325},
-
-        {x: 265, y: 380, width: 10, height: 105},
-        {x: 365, y: 380, width: 10, height: 75},
-        {x: 265, y: 380, width: 110, height: 10},
-        {x: 155, y: 420, width: 110, height: 10},
-
-        {x: 445, y: 360, width: 10, height: 75},
-        {x: 545, y: 360, width: 10, height: 140},
-        {x: 545, y: 360, width: 80, height: 10},
-        {x: 445, y: 490, width: 110, height: 10},
-
-        {x: 605, y: 180, width: 90, height: 10},
-        {x: 685, y: 20, width: 10, height: 170},
-
-        {x: 605, y: 180, width: 10, height: 180},
-    ];
-
 
     // Helper functions
     function scaleVector(scale, vector) {
@@ -95,6 +49,25 @@
         if (gravityDirection.y === 0 && gravityDirection.x < 0) {
             return (player.body.touching.left); // || player.body.touching.up || player.body.touching.down);
         }
+    }
+
+    function loadLevel(index) {
+        levelIndex = index;
+        var level = window._levels[index];
+        // Place platforms
+        if (platforms) platforms.destroy();
+        platforms = game.add.physicsGroup();
+        for (var i = 0; i < level.platforms.length; i++) {
+            var data = level.platforms[i];
+            var platform = platforms.create(data.x, data.y, 'platform');
+            platform.scale.setTo(data.width/100, data.height/100);
+        }
+        platforms.setAll('body.immovable', true);
+        // Place player & goal
+        player.position.setTo(level.start.x, level.start.y);
+        goal.position.setTo(level.goal.x, level.goal.y);
+        // Set initial gravity
+        setGravity(level.gravity[0]);
     }
 
     function setGravity(dir) {
@@ -122,7 +95,6 @@
 
     function goalReached() {
         console.log('YOU WIN');
-        player.body.position.setTo(START_POSITION.x, START_POSITION.y);
     }
 
     // Preload assets
@@ -138,31 +110,20 @@
         game.load.image('arrow', 'down-arrow.png');
     }
 
-    // Create the environment
+    // Create the game objects
     function create() {
         // The goal
-        goal = game.add.sprite(GOAL_POSITION.x, GOAL_POSITION.y, 'goal');
+        goal = game.add.sprite(0, 0, 'goal');
         game.physics.arcade.enable(goal);
 
         // The player
-        player = game.add.sprite(START_POSITION.x, START_POSITION.y, 'player');
-        window.player = player;
+        player = game.add.sprite(0, 0, 'player');
         player.animations.add('left', [0, 1, 2, 3], 10, true);
         player.animations.add('right', [5, 6, 7, 8], 10, true);
         // Set up player physics
         game.physics.arcade.enable(player);
         player.body.collideWorldBounds = true;
-        player.body.gravity.y = GRAVITY_STRENGTH;
         player.anchor.setTo(0.5, 0.5);
-
-        // Create platforms
-        platforms = game.add.physicsGroup();
-        for (var i = 0; i < PLATFORMS.length; i++) {
-            var data = PLATFORMS[i];
-            var platform = platforms.create(data.x, data.y, 'platform');
-            platform.scale.setTo(data.width/100, data.height/100);
-        }
-        platforms.setAll('body.immovable', true);
 
         // Create the warning arrow
         downArrow = game.add.sprite(stageWidth/2, stageHeight/2, 'arrow');
@@ -177,6 +138,9 @@
         // Set up gravity flipping timer
         flipTimer = game.time.events.add(TIME_TO_FLIP, warnAndFlip);
         flipTimer.loop = true;
+
+        // Load the first level
+        loadLevel(0);
     }
 
 
---
+++ src/levels.js
@@ -0,0 +1,39 @@
+/**
+ * Project 2 Game - Levels
+ *
+ * Forrest Moret & Eric Schmidt
+ * February 2016
+ */
+
+window._levels = [
+    // Level 0
+    {
+        start: {x: 50, y: 530},
+        goal: {x: 410, y: 130},
+        gravity: [
+            {x: 0, y: 1},
+        ],
+        platforms: [
+            {x: 0, y: 0, width: 800, height: 20},
+            {x: 0, y: 0, width: 20, height: 600},
+            {x: 780, y: 0, width: 20, height: 600},
+            {x: 0, y: 580, width: 800, height: 20},
+            {x: 325, y: 100, width: 175, height: 25},
+            {x: 325, y: 250, width: 175, height: 25},
+            {x: 500, y: 100, width: 25, height: 175},
+            {x: 250, y: 180, width: 200, height: 10},
+            {x: 225, y: 0, width: 25, height: 325},
+            {x: 265, y: 380, width: 10, height: 105},
+            {x: 365, y: 380, width: 10, height: 75},
+            {x: 265, y: 380, width: 110, height: 10},
+            {x: 155, y: 420, width: 110, height: 10},
+            {x: 445, y: 360, width: 10, height: 75},
+            {x: 545, y: 360, width: 10, height: 140},
+            {x: 545, y: 360, width: 80, height: 10},
+            {x: 445, y: 490, width: 110, height: 10},
+            {x: 605, y: 180, width: 90, height: 10},
+            {x: 685, y: 20, width: 10, height: 170},
+            {x: 605, y: 180, width: 10, height: 180},
+        ]
+    },
+];
